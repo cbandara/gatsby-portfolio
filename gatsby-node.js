ddd566a0b0caac42222a8296805af51a2e719d86
devin23 <devin@3droomspace.com>
2020-06-07 18:13:52 -0400

    added seo

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,7 +14,7 @@ module.exports.onCreateNode = ({ node, actions }) => {
   }
 }
 
-module.exports.createPages = ({ graphql, actions }) => {
+module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   // Get path to template
