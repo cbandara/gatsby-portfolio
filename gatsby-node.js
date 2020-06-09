de90d8892cae712095572201e94a8c0c1355ad5f
devin23 <devin@3droomspace.com>
2020-06-08 21:52:39 -0400

    Revert "contentful images"
    This reverts commit 33f2c29987720c203c088b6510bc9310676bde77.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,19 @@
 const path = require('path')
 
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug
+    })
+  }
+}
+
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
@@ -9,23 +23,25 @@ module.exports.createPages = async ({ graphql, actions }) => {
   // get markdown data
   const res = await graphql(`
     query {
-      allContentfulBlogPost {
-        edges {
-          node {
-            slug
+    allMarkdownRemark {
+      edges {
+        node {
+          fields{ slug }
           }
         }
       }
     }
 `)
 
-  res.data.allContentfulBlogPost.edges.forEach((edge) => {
+  res.data.allMarkdownRemark.edges.forEach((edge) => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.slug}`,
+      path: `/blog/${edge.node.fields.slug}`,
       context: {
-        slug: edge.node.slug
+        slug: edge.node.fields.slug
       }
     })
   })
+
+  // create new pages
 }
