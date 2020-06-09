210a8d8567d53fd5d97b8768e68b1dfc00bd9511
devin23 <devin@3droomspace.com>
2020-06-08 21:56:23 -0400

    Revert "Revert "blog styling""
    This reverts commit 74a418e8a27ea126640ba842eaa15ce0d85eb58e.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
 
+import blogStyles from '../styles/blog.module.scss'
+
 export const query = graphql`
 query($slug: String!) {
   markdownRemark(fields: {slug: { eq: $slug}}) {
@@ -17,9 +19,13 @@ query($slug: String!) {
 const Blog = (props) => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+      <div className="container">
+        <div className={blogStyles.blogContainer}>
+          <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+          <p>{props.data.markdownRemark.frontmatter.date}</p>
+          <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+        </div>
+      </div>
     </Layout>
   )
 }
